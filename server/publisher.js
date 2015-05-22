f44695d2bb7ab29fbdd3178afcaafda9f695631e
Andrea Mannarà <a.mannara@qualitech.it>
2015-05-22 17:59:45 +0200

    Gestita aggiunta di lovebox iniziata aggiunta mp3

--- server/publisher.js
+++ server/publisher.js
@@ -1,3 +1,9 @@
 Meteor.publish('loveboxes',function(){
-  return Loveboxes.find({ owner: Meteor.user()._id });
+  return Loveboxes.find();
+});
+Meteor.publish('mp3s',function(){
+  return Mp3s.find();
+});
+Meteor.publish('mbins',function(){
+  return MBins.find();
 });
